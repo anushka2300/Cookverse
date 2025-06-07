5e0bfd546a287f2578023b64c74f0ec77973669e
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2025-06-07 22:41:48 +0530

    frontend

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Mail, Lock, Eye, EyeOff } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
 
 const Login = () => {
