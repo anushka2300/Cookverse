7a04b51e8ca4950409ccf60e56f2f9b84a68da7c
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2025-06-21 00:47:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const MongoStore = require('connect-mongo');
 const bodyParser = require('body-parser');
 const cors=require('cors');
 const corsOptions = {
-  origin: ['http://localhost:5173','https://cookverse.vercel.app','https://cookverse-frontend.onrender.com'], 
+  origin: ['http://localhost:5173','https://cookverse-frontend.onrender.com'], 
   credentials: true
 };
 app.use(cors(corsOptions));
